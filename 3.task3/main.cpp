d0c5102950283fc75549e44407503304a9dbef60
Noorulainfaisal838 <faisalnoorulain7@gmail.com>
2024-11-30 10:31:49 +0500

    first commit

---
+++ 3.task3/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int sum=0;
+    int arr[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++)
+        {cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            sum+=arr[i][j];
+ }
+    }
+cout<<"the sum is:"<<sum;
+}
